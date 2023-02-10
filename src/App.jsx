1d94b8ca7479d0fd7a67f03053506ce0e5479222
SirCodr <jotarestrepo01@gmail.com>
2023-02-10 11:52:05 -0500

    Wrap app component in Router provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import Tasks from './components/task'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Welcome from './components/Welcome'
+import TaskList from './components/task/TaskList'
+import TaskForm from './components/task/TaskForm'
+import Layout from './components/Layout'
 
 function App () {
   return (
     <div className='App'>
-      <Tasks />
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path='/' element={<Welcome />} />
+            <Route path='task' element={<TaskList />} />
+            <Route path='task/new' element={<TaskForm />} />
+            <Route path='task/edit/:id' element={<TaskForm />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
     </div>
   )
 }
