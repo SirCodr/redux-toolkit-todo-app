d4a9a16cc015c3e4bcea827fa7879d0be3efd262
SirCodr <jotarestrepo01@gmail.com>
2023-02-10 09:53:53 -0500

    Create task slice

---
+++ src/features/task/taskSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+import taskReducer from './taskReducer'
+
+const initialState = [
+  {
+    id: '0001',
+    title: 'Primera tarea',
+    description: 'Esta es una tarea generada automaticamente'
+  }
+]
+
+export const taskSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: taskReducer
+})
+
+export const { addTask, deleteTask } = taskSlice.actions
+
+export default taskSlice.reducer
