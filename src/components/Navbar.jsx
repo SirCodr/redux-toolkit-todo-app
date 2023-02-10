e5536c02a0a56e3c00178be17a05cadfcab3a8d8
SirCodr <jotarestrepo01@gmail.com>
2023-02-10 11:52:23 -0500

    Add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <header className='flex gap-x-8 p-4'>
+      <Link to='/'>Home</Link>
+      <Link to='/task'>Tareas</Link>
+      <Link to='/task/new'>Crear tarea</Link>
+    </header>
+  )
+}
+export default Navbar
