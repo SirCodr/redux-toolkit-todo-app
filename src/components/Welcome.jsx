62fd3aab43f03a2ea39509b9dca9df8323a71cda
SirCodr <jotarestrepo01@gmail.com>
2023-02-10 15:32:53 -0500

    Refactor welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,15 @@
-import { Link } from 'react-router-dom'
-
 const Welcome = () => {
   return (
-    <div>
-      Welcome
-      <div className='flex gap-x-4'>
-        <Link to='task'>Tareas</Link>
-        <Link to='task/new'>Crear tarea</Link>
+    <div className='w-full'>
+      <h4 className='font-medium text-6xl'>WELCOME</h4>
+      <p className='text-2xl'>This a todo list app with:</p>
+      <div>
+        <ul>
+          <li><small>Redux toolkit</small></li>
+          <li><small>React</small></li>
+          <li><small>React router</small></li>
+          <li><small>Tailwind</small></li>
+        </ul>
       </div>
     </div>
   )
